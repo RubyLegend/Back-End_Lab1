79aff6c8722d1349e06568aa9faecbde28213fdc
Serhii Hatsan <shatsanik01@office365.fiot.kpi.ua>
2022-10-22 19:58:42 +0300

    Removed dependency on json lib. Added /users endpoint.

--- stealth_webpage/views.py
+++ stealth_webpage/views.py
@@ -6,28 +6,46 @@
 #   /categories
 #
 
-import json
 from flask import abort, jsonify, request
 from stealth_webpage import app
 
 categories = []
+users = [{"id": 0, "name": "admin"}]
 
-@app.route("/categories", methods=['GET', 'POST'])
+@app.route("/categories", methods=['GET'])
 def retrieve_categories():
-    if request.method == 'GET':
-        deserialized = []
-        for element in categories:
-            deserialized.append(json.loads(element))
+    return jsonify({"categories": categories})
 
-        return jsonify({"categories": deserialized})
-    else:
-        data = dict(request.get_json())  # type: ignore
-        if len(data) == 2 and 'id' in data and 'name' in data:
-            categories.append(json.dumps(data))
-            return jsonify({"success": "Ok", "data": data})
-        elif len(data) == 1 and 'name' in data:
-            data['id'] = json.loads(categories[len(categories)-1])['id']+1 # Getting id of last record and incrementing it
-            categories.append(json.dumps(data))
-            return jsonify({"success": "Ok", "data": data})
+@app.route("/categories", methods=['POST'])
+def create_categories():
+    data = dict(request.get_json())  # type: ignore
+    if len(data) == 2 and 'id' in data and 'name' in data:
+        categories.append(data)
+        return jsonify({"success": "Ok", "data": data})
+    elif len(data) == 1 and 'name' in data:
+        if len(categories) == 0:
+            data['id'] = 1
         else:
-            abort(400)
+            data['id'] = categories[len(categories)-1]['id']+1 # Getting id of last record and incrementing it
+        categories.append(data)
+        return jsonify({"success": "Ok", "data": data})
+    else:
+        abort(400) 
+
+@app.route("/users", methods=['GET'])
+def enumerate_user():
+    return jsonify({"users": users})
+    
+@app.route("/users", methods=['POST'])
+def create_user():
+    data = dict(request.get_json())  # type: ignore
+    if len(data) == 2 and 'id' in data and 'name' in data:
+        users.append(data)
+        return jsonify({"success": "Ok", "data": data})
+    elif len(data) == 1 and 'name' in data:
+        data['id'] = users[len(users)-1]['id']+1 # Getting id of last record and incrementing it
+        users.append(data)
+        return jsonify({"success": "Ok", "data": data})
+    else:
+        abort(400) 
+    
