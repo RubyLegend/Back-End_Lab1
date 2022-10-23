276a10a5b705a9fe393608b5592720e3814f55d0
Serhii Hatsan <shatsanik01@office365.fiot.kpi.ua>
2022-10-23 12:13:09 +0300

    Added /records endpoint.

--- stealth_webpage/views.py
+++ stealth_webpage/views.py
@@ -6,11 +6,22 @@
 #   /categories
 #
 
+from datetime import datetime
+from random import random
 from flask import abort, jsonify, request
 from stealth_webpage import app
 
-categories = []
+categories = [{"id": 0, "name": "test_category"}]
 users = [{"id": 0, "name": "admin"}]
+records = [
+        {
+            "id":0,
+            "userId": 0,
+            "categoryId": 0,
+            "date_time": datetime.now().strftime("%d/%m/%y %H:%M"),
+            "total": (int)(random()*1000)
+        }
+]
 
 @app.route("/categories", methods=['GET'])
 def retrieve_categories():
@@ -49,3 +60,29 @@ def create_user():
     else:
         abort(400) 
     
+@app.route("/records", methods=['GET'])
+def get_records():
+    return jsonify({"records": records})
+
+@app.route("/records", methods=['POST'])
+def create_record():
+    data = dict(request.get_json())  # type: ignore
+    if len(data) == 5 and 'id' in data and 'userId' in data\
+                      and 'categoryId' in data and 'date_time' in data and 'total' in data:
+        data['date_time'] = datetime.strptime(data['date_time'], "%d/%m/%y %H:%M")
+        records.append(data)
+        return jsonify({"success": "Ok", "data": data})
+    elif len(data) == 4 and 'userId' in data and 'categoryId' in data \
+                        and 'date_time' in data and 'total' in data:
+        data['id'] = users[len(users)-1]['id']+1 # Getting id of last record and incrementing it
+        data['date_time'] = datetime.strptime(data['date_time'], "%d/%m/%y %H:%M")
+        users.append(data)
+        return jsonify({"success": "Ok", "data": data})
+    elif len(data) == 3 and 'userId' in data and 'categoryId' in data \
+                        and 'total' in data:
+        data['id'] = users[len(users)-1]['id']+1 # Getting id of last record and incrementing it
+        data['date_time'] = datetime.now()
+        users.append(data)
+        return jsonify({"success": "Ok", "data": data})
+    else:
+        abort(400) 
